fa840093e03ad572d41cc9f1102fa8f906dba463
Eugene Leonkov <eleonkov@gmail.com>
2022-06-12 15:45:34 +0200

    update app

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,4 +1,9 @@
 import React from "react";
 import { MarketingApp } from "./components/MarketingApp";
 
-export default () => <MarketingApp />;
+export default () => (
+  <>
+    <h1>He there!</h1>
+    <MarketingApp />
+  </>
+);
